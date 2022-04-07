be5d537b50a7c41aabceba0e17d2ec2997858181
BYSoooooo <avera0128@gmail.com>
2022-04-07 22:02:12 +0900

    Initial Setting for Maven

---
+++ buyforall/src/main/java/com/example/buyforall/BuyforallApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyforallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyforallApplication.class, args);
+	}
+
+}
