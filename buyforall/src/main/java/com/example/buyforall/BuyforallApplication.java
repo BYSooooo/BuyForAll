91345ffcb7f6e72ea1baa07a87e99a77bc17f30b
BYSoooooo <avera0128@gmail.com>
2022-04-11 17:12:32 +0900

    Apply Thymeleaf Template View

--- buyforall/src/main/java/com/example/buyforall/BuyforallApplication.java
+++ buyforall/src/main/java/com/example/buyforall/BuyforallApplication.java
@@ -2,8 +2,10 @@ package com.example.buyforall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example")
 public class BuyforallApplication {
 
 	public static void main(String[] args) {
