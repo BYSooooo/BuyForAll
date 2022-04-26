d99b1129756aab47a981bea79e2ef3c6f8ede5da
BYSoooooo <avera0128@gmail.com>
2022-04-26 23:18:49 +0900

    Modify CSS

--- buyforall/src/main/resources/static/javascript/member/joinMemberForm.js
+++ buyforall/src/main/resources/static/javascript/member/joinMemberForm.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    var BtnToSeStatus = document.getElementById('toJoinSe');
     // 가입 과정 중 Email 중복 검사
     document.getElementById('checkEmail').addEventListener('click',() => {
         var memberEmail = document.getElementById("memberEmail").value;
@@ -11,7 +12,6 @@ window.onload = function () {
                 var requestResult = httpRequest.response;
                 if (httpRequest.status == 200) {
                     var verifyNumber = requestResult.veriNum;
-                    var BtnToSeStatus = document.getElementById('toJoinSe');
                     BtnToSeStatus.disabled =  false;
                     console.log(verifyNumber);
                 } else {
@@ -25,5 +25,16 @@ window.onload = function () {
         httpRequest.open('POST', '/checkDBEmail?memberEmail=' + memberEmail);
         httpRequest.responseType = "json";
         httpRequest.send();
+    })
+    
+    /* sectionOne 에서 '다음' 클릭 시 sectionTwo로 스크롤 */
+    document.getElementById('toJoinSe').addEventListener('click', () => {
+        document.getElementById('sectionTwo').scrollIntoView({behavior:'smooth',inline:'start'})
+
+    })
+    /* sectionTwo에서 '이전' 클릭 시 sectionOne로 스크롤 + '다음' 버튼 비 활성화 */
+    document.getElementById('beforeButton').addEventListener('click',() => {
+        document.getElementById('sectionOne').scrollIntoView({behavior:'smooth', inline:'end'})
+        BtnToSeStatus.disabled=true;
     })
 }
